f98c9f1523c125be4249b5d0c399c5b771eaff21
e8e4caivtw <kemplar285@gmail.com>
2023-10-10 11:22:06 +0300

    initial commit

---
+++ src/main/java/com/shop/webstore/WebstoreApplication.java
@@ -0,0 +1,13 @@
+package com.shop.webstore;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WebstoreApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WebstoreApplication.class, args);
+	}
+
+}
